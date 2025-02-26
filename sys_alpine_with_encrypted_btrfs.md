699dae84883b7636f0c9c4e959214212db38293b
Erik Schlyter <erik@erisc.se>
2025-02-26 09:30:51 +0100

    Sys install with encrypted BTRFS and swap.

---
+++ sys_alpine_with_encrypted_btrfs.md
@@ -0,0 +1,46 @@
+Alpine Linux system on encrypted BTRFS root, with encrypted swap and boot (GRUB)
+================================================================================
+
+This document explains how to perform a Alpine Linux system installation with
+the following features:
+
+- Encrypted BTRFS on root, with distinct submodules.
+- Encrypted swap disk for RAM.
+
+The reason to use this script instead of the default procedure in `setup-disk`
+is because it mounts root to a BTRFS subvolume, and it encrypts swap on a
+separate partition. It's also easier to setup/configure additional partitions.
+
+Base installation
+-----------------
+Download [Alpine Linux Standard](https://alpinelinux.org/downloads/), write it
+to a USB stick and boot up on the target machine. Execute the classic
+`setup-alpine`, but choose `none` on the disk options. This will setup the
+environment to your liking.
+
+Get the scripts into the new target machine
+-------------------------------------------
+You need to install `git` to clone this repo, and `lsblk` to check the correct
+disk device for USB.
+
+    apk add git lsblk
+
+You can either clone this repo from a public source:
+
+    git clone https://github.com/ErikSchlyter/alpine-scripts.git
+    cd alpine-scripts
+
+...or, you can serve the repo (along with your own custom configuration) from
+another machine on the local network using `python3 -m http.server`. Just make
+sure you have executed `git update-server-info` in your host repo first.
+
+    git clone http://yourhostip:8000/.git alpine-scripts
+    cd alpine-scripts
+
+System installation to disk
+---------------------------
+Execute the script with the device as argument, followed by the desired BTRFS
+subvolume paths:
+
+    ./sys_alpine_with_encrypted_btrfs.sh /dev/nvme0n0 /var/cache /var/log /home
+
---
+++ sys_alpine_with_encrypted_btrfs.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+
+usage="
+Installs Alpine on encrypted BTRFS using encrypted swap.
+
+Usage:
+
+    ./sys_alpine_with_encrypted_btrfs.sh <disk> [dir ...]
+
+The script assumes you are running a fresh setup of Alpine Linux and just
+executed 'setup-alpine', with 'none' on all the disk related options.
+
+The script will wipe the given disk and create three partitions: an EFI boot
+partition, a partition for swap, and the partition for the root filesystem that
+will use BTRFS. It will create additional BTRFS subvolumes for each directory
+specified as argument.
+
+The following environment variables are used:
+
+- BTRFS_OPTS: the BTRFS options
+- EFI_SIZE: the size of the efi partition
+- SWAP_SIZE: the size of the swap partition
+
+Example:
+
+    ./sys_alpine_with_encrypted_btrfs.sh /dev/sda /var/cache /var/log /home
+
+"
+disk="${1}"
+shift
+subvolume_dirs="/ $@"
+
+BTRFS_OPTS="${BTRFS_OPTS:-defaults,noatime,nodiratime,discard=async,space_cache=v2,compress=zstd}"
+EFI_SIZE="${EFI_SIZE:-300M}"
+SWAP_SIZE="${SWAP_SIZE:-1G}"
+
+# the crypto keyfile path is hardcoded in mkinitfs (doh!), which means we should
+# probably stick to this path for future endeavours.
+crypto_keyfile="/crypto_keyfile.bin"
+
+if [ ! -e "$disk" ] ; then
+    echo "$usage"
+    exit 1
+fi
+
+# Install required packages
+apk add sgdisk dosfstools cryptsetup btrfs-progs blkid
+modprobe btrfs
+
+# Zap the entire disk, and create EFI, swap, and system partitions
+sgdisk --zap-all $disk
+sgdisk -n 1:0:+$EFI_SIZE  -t 0:ef00 $disk
+sgdisk -n 2:0:+$SWAP_SIZE -t 0:8200 $disk
+sgdisk -n 3:0:0           -t 0:8300 $disk
+mdev -s
+
+# Get device path for each of the partitions
+function device_path_for_partition {
+    # turns "/dev/nvme0n0" "1" into /dev/nvme0n0p1"
+    echo "$(echo "${1}" | sed "s/\([0-9]\)$/\1p/")$2"
+}
+efi_part="$(device_path_for_partition $disk 1)"
+swap_part="$(device_path_for_partition $disk 2)"
+system_part="$(device_path_for_partition $disk 3)"
+
+# Create the LUKS container for the root file system
+./create_luks_container.sh $system_part $crypto_keyfile root
+
+# Create the file system
+partition="/dev/mapper/root"
+mkfs.btrfs -f $partition
+
+function subvolume {
+    # turns e.g. "/var/log" into "@var_log"
+    echo "@$(echo ${1/\//} | sed "s#/#_#g")"
+}
+
+# Create the subvolumes
+mount $partition /mnt
+for dir in $subvolume_dirs; do
+    btrfs su cr /mnt/$(subvolume $dir)
+done
+umount /mnt
+
+# Mount the subvolumes with the given BTRFS options
+for dir in $subvolume_dirs; do
+    mkdir -v -p /mnt/${dir/\//}
+    mount -o "${BTRFS_OPTS},subvol=$(subvolume $dir)" $partition /mnt${dir%/}
+done
+
+# Setup the boot partition
+efi_dir=/boot
+mkfs.vfat -F32 $efi_part
+mkdir -v -p /mnt$efi_dir
+mount -t vfat $efi_part /mnt$efi_dir
+
+# Enable dmcrypt so we can decrypt swap
+rc-update add dmcrypt boot
+
+# Setup encrypted swap
+./encrypted_swap.sh $swap_part $crypto_keyfile swap
+
+# Install Alpine on disk
+export SWAP_DEVICES=/dev/mapper/swap
+setup-disk -m sys /mnt/
+
+
+# Adding passphrase to the LUKS system container, so it can be unlocked by
+# passphrase during boot. Note that we do this after we have created the LUKS
+# containers and the crypto keyfile, since we want the key file (with low PBKDF
+# itations) in the first slot, to make it quick to unlock remaining partitions.
+cryptsetup luksAddKey --key-file $crypto_keyfile $system_part
+
+# Move the keyfile to the new system, this is quite important, so that it can be
+# used when decrypting swap
+mv -v $crypto_keyfile "/mnt${crypto_keyfile}"
